6a7d53fb790f15205b750f2d0f590a87a5c44952
ChenFeng <z.chenfengzhang@gmail.com>
2020-04-07 12:00:11 -0400

    added some shorthand stuffs

--- bot.py
+++ bot.py
@@ -10,42 +10,55 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='-')
 
-msg_log = {}
+msg_log = queue.LifoQueue()
+shortcut = {
+    'kc': 'kantai_collection',
+    'al': 'azur_lane',
+    'gf': 'girls_frontline',
+    'ak': 'arknights',
+    'pk': 'pokemon'
+    'fgo': 'fate/grand_order'
+    }
 
 @bot.event
 async def on_ready():
     print("{} connected".format(bot.user))
 
 
-@bot.command(name='search')
+@bot.command(name='search', description="search for an image on danbooru\nlimit to 2 tags because I am poor")
 async def search(ct, *args):
-    response = websearch.danbooruSearch(args)
+    cleaned = list(map(short, args))
+    response = websearch.danbooruSearch(cleaned)
     if response:
         sent = await ct.send(response)
-        try:
-            msg_log[ct.author.id].put(sent)
-        except KeyError:
-            msg_log[ct.author.id] = queue.LifoQueue()
-            msg_log[ct.author.id].put(sent)
-    else: 
+        msg_log.put(sent)
+    else:
         await ct.send("nothing found")
 
 
-@bot.command(name='del')
+@bot.command(name='del', description="deletes the last image sent")
 async def delete(ct):
     try:
-        msg = msg_log[ct.author.id].get(0)
-    except KeyError:
-        pass
+        msg = msg_log.get(0)
+        await msg.delete()
     except queue.Empty:
         pass
-    else:
-        await msg.delete()
 
 
 @bot.command(name='exec')
 async def ex(ct, cmd):
     exec(cmd)
+
+
+def short(x):
+    a = x.split(",")
+    if len(a) == 2:
+        try:
+            a[1] = shortcut[a[1]]
+        except KeyError:
+            pass
+        return f"{a[0]}_({a[1]})"
+    return x
 
 
 bot.run(TOKEN)
